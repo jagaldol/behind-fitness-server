c26c1bc90a73c69a5d9c9a51a607c58b1b8d1a22
jagaldol <yoy06056@pusan.ac.kr>
2024-04-19 17:49:51 +0900

    chore: ddl 생성

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,46 @@
+CREATE TABLE user_tb
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(20)  NOT NULL,
+    email      VARCHAR(100) NOT NULL UNIQUE,
+    password   VARCHAR(100) NOT NULL,
+    memo       VARCHAR(100),
+    created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE sport_tb
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT NOT NULL,
+    name       VARCHAR(20),
+    created_at TIMESTAMP DEFAULT now(),
+    UNIQUE (user_id, name)
+);
+
+CREATE TABLE workout_session_tb
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT NOT NULL,
+    date       DATE   NOT NULL,
+    start      TIME,
+    end        TIME,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE workout_record_tb
+(
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
+    workout_session_id BIGINT NOT NULL,
+    sport_id           BIGINT NOT NULL,
+    created_at         TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE set_record_tb
+(
+    id                BIGINT AUTO_INCREMENT PRIMARY KEY,
+    workout_record_id BIGINT NOT NULL,
+    weight            INT        DEFAULT 0,
+    count             INT        DEFAULT 0,
+    countUnit         VARCHAR(5) DEFAULT '회',
+    created_at        TIMESTAMP  DEFAULT now()
+);
